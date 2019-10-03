a8ee9b3313c771c514ef38e3ead029bcf5627893
Nord18 <andrey.zhilyaev@gmail.com>
2019-10-03 15:35:25 +0300

    update gitignore file

---
+++ build/precache-manifest.a47c297b9eec2640a48955e062adfd5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9702622e01b8f99cd1c51cba976038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c168d108ee03a6b3437",
+    "url": "/static/css/main.795b91e1.chunk.css"
+  },
+  {
+    "revision": "9f51a7c84254a2376d01",
+    "url": "/static/js/2.b778b692.chunk.js"
+  },
+  {
+    "revision": "3c168d108ee03a6b3437",
+    "url": "/static/js/main.80bc8c36.chunk.js"
+  },
+  {
+    "revision": "c9e49bd588f993a7d757",
+    "url": "/static/js/runtime~main.189ddb5c.js"
+  }
+]);
